38a1dab8f88805ee4f964fd37458b3b83cc9abc7
brendanlyc <brendanlyc@gmail.com>
2024-04-24 20:08:13 +0800

    merged weekly and daily sentiment pipelines

--- dags/sentiment_score_pipeline.py
+++ dags/sentiment_score_pipeline.py
@@ -19,63 +19,11 @@ engine = create_engine(connection_string)
 Scraps Reddit headlines daily, and generate an aggregate sentiment score for that day
 """
 
-@dag(dag_id='daily_sentiment_score_etl_pipeline', start_date = datetime(2024,4,15),schedule_interval= '0 0 * * *', catchup=False) #runs once a day at midnight
-def daily_sentiment_score_etl_pipeline():
+@dag(dag_id='sentiment_score_pipeline', start_date = datetime(2024,4,15),schedule_interval= '0 0 * * *', catchup=False) #runs once a day at midnight
+def sentiment_score_pipeline():
 	@task
-	def create_table():
-		create_table_sql = """
-        	CREATE SCHEMA IF NOT EXISTS sentiment_data;
-            CREATE TABLE IF NOT EXISTS sentiment_data.daily_sentiment (
-                date DATE PRIMARY KEY,
-                sentiment_score FLOAT,
-                sentiment VARCHAR
-            );
-        """
-
-		create_table_sql_2 = """
-			CREATE SCHEMA IF NOT EXISTS sentiment_data;
-            CREATE TABLE IF NOT EXISTS sentiment_data.cumulative_mean_sentiment (
-            	id SERIAL PRIMARY KEY,
-                mean_daily_sentiment FLOAT,
-				num_days_daily_mean INTEGER,
-                mean_weekly_sentiment FLOAT,
-				num_days_weekly_mean INTEGER 
-            );
-        """
-
-		with engine.connect() as connection:
-			connection.execute(create_table_sql)
-			connection.execute(create_table_sql_2)
-
-	@task
-	def fetch_reddit_data():
-		reddit = praw.Reddit(
-	        client_id='MwjT-kwEq6x48sV78Tcw7w',
-	        client_secret='eTgMh4hzhXOnoGp-5j1Rc_b2H6XyRA',
-	        user_agent='IS3107'
-	    )
-
-		listofsubreddits = ['wallstreetbets','investing','stocks','worldnews']
-
-		posts_data = []
-
-		for subreddit in listofsubreddits:
-			current_subreddit = reddit.subreddit(subreddit)
-
-			for submission in current_subreddit.hot(limit = 25):
-				posts_data.append([submission.title])
-
-		df = pd.DataFrame(posts_data, columns=['Title'])
-
-		folder_path = os.path.join(os.getcwd(),'intermediate_files')
-		os.makedirs(folder_path,exist_ok=True)
-		csv_file_path = os.path.join(folder_path,'reddit_headlines.csv')
-
-		df.to_csv(csv_file_path, index=False)
-		return csv_file_path
-
-	@task
-	def generate_and_load_sentiment_score(file_path):
+	def generate_and_load_daily_sentiment_score():
+		#helper functions
 		def remove_stopwords(sentence):
 			sentence = re.sub(r'https?://\S+|www\.\S+', '',sentence)
 			sentence = re.sub(r'<.*?>', '', sentence)
@@ -99,8 +47,24 @@ def daily_sentiment_score_etl_pipeline():
 				return "NEGATIVE"
 			else:
 				return "NEUTRAL"
+			
+		reddit = praw.Reddit(
+	        client_id='MwjT-kwEq6x48sV78Tcw7w',
+	        client_secret='eTgMh4hzhXOnoGp-5j1Rc_b2H6XyRA',
+	        user_agent='IS3107'
+	    )
 
-		df = pd.read_csv(file_path)
+		listofsubreddits = ['wallstreetbets','investing','stocks','worldnews']
+
+		posts_data = []
+
+		for subreddit in listofsubreddits:
+			current_subreddit = reddit.subreddit(subreddit)
+
+			for submission in current_subreddit.hot(limit = 25):
+				posts_data.append([submission.title])
+
+		df = pd.DataFrame(posts_data, columns=['Title'])
 
 		stopwords = nltk.corpus.stopwords.words("english")
 		sia = SentimentIntensityAnalyzer()
@@ -130,7 +94,7 @@ def daily_sentiment_score_etl_pipeline():
 			)
 
 	@task
-	def update_mean_sentiment_score():
+	def update_daily_mean_sentiment_score():
 
 		get_latest_sentiment_query = """
 		SELECT sentiment_score
@@ -172,15 +136,95 @@ def daily_sentiment_score_etl_pipeline():
 				)
 				"""
 			)
+
+	@task
+	def generate_and_load_weekly_sentiment_score():
+		start_of_week = datetime.date(datetime.now() - timedelta(days=7))
+		end_of_week = start_of_week + timedelta(days=6)
+
+		sql_check_count_query = f"SELECT COUNT(*) FROM sentiment_data.daily_sentiment WHERE date BETWEEN '{start_of_week}' AND '{end_of_week}'"
+		total_count = pd.read_sql(sql_check_count_query, engine).iloc[0, 0]
+
+		if total_count == 7:
+			sql_query = f"SELECT sentiment_score FROM sentiment_data.daily_sentiment WHERE date BETWEEN '{start_of_week}' AND '{end_of_week}'"
+			df = pd.read_sql(sql_query,engine)
+
+			weekly_sentiment_score = df['sentiment_score'].mean()
+
+			if weekly_sentiment_score > 0.05:
+				weekly_sentiment = "POSITIVE"
+			elif weekly_sentiment_score < -0.05:
+				weekly_sentiment = "NEGATIVE"
+			else:
+				weekly_sentiment = "NEUTRAL"
+			
+			new_row_in_df = pd.DataFrame([[start_of_week,end_of_week,weekly_sentiment_score,weekly_sentiment]],columns=['start_date','end_date','sentiment_score','sentiment'])
 		
+			new_row_in_df.to_sql('weekly_sentiment',con=engine,schema='sentiment_data',if_exists='append',index=False)
 
-	create_table_task = create_table()
-	reddit_data_file_path = fetch_reddit_data()
-	generate_and_load_sentiment_score_task = generate_and_load_sentiment_score(reddit_data_file_path)
-	update_mean_sentiment_score_task = update_mean_sentiment_score()
+		#remove old entries if more than 3
+		with engine.connect() as connection:
+			connection.execute(
+				"""
+				DELETE FROM sentiment_data.weekly_sentiment
+				WHERE end_date IN (
+					SELECT end_date
+					FROM sentiment_data.weekly_sentiment
+					ORDER BY end_date DESC
+					LIMIT 3 OFFSET 3
+				)
+				"""
+			)
 
-	create_table_task >> reddit_data_file_path >> generate_and_load_sentiment_score_task >> update_mean_sentiment_score_task
+	@task
+	def update_weekly_mean_sentiment_score():
 
-run_dag = daily_sentiment_score_etl_pipeline()
+		get_latest_sentiment_query = """
+		SELECT sentiment_score
+		FROM sentiment_data.weekly_sentiment
+		ORDER BY end_date DESC
+		LIMIT 1
+		"""
+		latest_sentiment_score = pd.read_sql(get_latest_sentiment_query,engine).iloc[0,0]
+
+		get_latest_cume_query = """
+		SELECT *
+		FROM sentiment_data.cumulative_mean_sentiment
+		"""
+		get_cume_data = pd.read_sql(get_latest_cume_query,engine)
+
+		updated_cume_data = {
+			'mean_daily_sentiment': get_cume_data['mean_daily_sentiment'],
+			'num_days_daily_mean': get_cume_data['num_days_daily_mean'],
+			'mean_weekly_sentiment': ((get_cume_data['mean_weekly_sentiment']*get_cume_data['num_days_weekly_mean'])+latest_sentiment_score)/(get_cume_data['num_days_weekly_mean'] + 1),
+			'num_days_weekly_mean': get_cume_data['num_days_weekly_mean'] + 1,
+		}
+
+		df = pd.DataFrame(updated_cume_data)
+
+		df.to_sql('cumulative_mean_sentiment',con=engine,schema='sentiment_data',if_exists='append',index=False)
+
+		with engine.connect() as connection:
+			connection.execute(
+				"""
+				DELETE FROM sentiment_data.cumulative_mean_sentiment
+				WHERE id NOT IN (
+					SELECT id
+					FROM sentiment_data.cumulative_mean_sentiment
+					ORDER BY id DESC
+					LIMIT 1
+				)
+				"""
+			)
+
+	generate_and_load_daily_sentiment_score_task = generate_and_load_daily_sentiment_score()
+	update_daily_mean_sentiment_score_task = update_daily_mean_sentiment_score()
+	generate_and_load_weekly_sentiment_score_task = generate_and_load_weekly_sentiment_score()
+	update_weekly_mean_sentiment_score_task = update_weekly_mean_sentiment_score()
+
+	generate_and_load_daily_sentiment_score_task >> [update_daily_mean_sentiment_score_task,generate_and_load_weekly_sentiment_score_task]
+	generate_and_load_weekly_sentiment_score_task >> update_weekly_mean_sentiment_score_task
+
+run_dag = sentiment_score_pipeline()
 
 
